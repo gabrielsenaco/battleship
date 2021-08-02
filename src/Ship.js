2cdad55817355a3e911b3c9773639c6787023d67
Gabriel <gabesenacom@gmail.com>
2021-08-02 10:35:29 -0300

    Add Ship object

---
+++ src/Ship.js
@@ -0,0 +1,46 @@
+export const Ship = (length, horizontal = true) => {
+  const parts = _build()
+
+  function _build () {
+    const array = []
+    for (let i = 0; i < length; i++) {
+      array.push(false)
+    }
+    return array
+  }
+
+  function hit (position) {
+    if (position < 0) return
+    parts[position] = true
+    return true
+  }
+
+  function isSunk () {
+    return getHits() === length
+  }
+
+  function getLength () {
+    return parts.length
+  }
+
+  function getParts () {
+    return parts
+  }
+
+  function getHits () {
+    return parts.filter((part) => part).length
+  }
+
+  function isHorizontal () {
+    return horizontal
+  }
+
+  return {
+    getLength,
+    isHorizontal,
+    getParts,
+    hit,
+    getHits,
+    isSunk
+  }
+}
