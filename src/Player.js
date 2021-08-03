f38e78490d3cfca0eca974ae9aea60593bd40b01
Gabriel <gabesenacom@gmail.com>
2021-08-02 22:43:53 -0300

    Add ComputerPlayer, that works like a bot, with random attack and random building scheme # TO DO Soon: Add smart AI that identify the hitted ship part and focus to discover all ships parts

--- src/Player.js
+++ src/Player.js
@@ -26,3 +26,56 @@ export const Player = () => {
     getEnemyGameboard
   }
 }
+
+export const ComputerPlayer = () => {
+  const { attack, getGameboard, getEnemyGameboard } = Player()
+
+  function getRandomTrueFalse () {
+    return Math.random() >= 0.5
+  }
+
+  function getRandomPosition () {
+    return Math.round(Math.random() * 9)
+  }
+
+  function buildGameboardScheme (shipListPattern) {
+    for (const shipPattern of shipListPattern) {
+      const horizontal = getRandomTrueFalse()
+      let x = getRandomPosition()
+      let y = getRandomPosition()
+      const ship = Ship(shipPattern.length, horizontal)
+      while (true) {
+        try {
+          getGameboard().placeShip(ship, x, y)
+          break
+        } catch (_) {
+          x = getRandomPosition()
+          y = getRandomPosition()
+        }
+      }
+    }
+  }
+
+  function betterRandomAttack () {
+    let response = {}
+    let position = null
+
+    while (!position || position.hitted) {
+      response = { x: getRandomPosition(), y: getRandomPosition() }
+      position = getEnemyGameboard().getPosition(response.x, response.y)
+    }
+    return response
+  }
+
+  function betterAttack () {
+    return betterRandomAttack()
+  }
+
+  return {
+    attack,
+    getGameboard,
+    getEnemyGameboard,
+    buildGameboardScheme,
+    betterAttack
+  }
+}
