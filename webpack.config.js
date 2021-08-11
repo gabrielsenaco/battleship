32a6ebe1e8490796b5ad878261137914c95bdd11
Gabriel <gabesenacom@gmail.com>
2021-08-11 19:14:58 -0300

    Add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'production',
+  entry: './src/js/index.js',
+  mode: 'development',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
@@ -14,5 +16,34 @@ module.exports = {
     watchContentBase: false,
     liveReload: true,
     port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }]
+  },
+  plugins: [
+    new MiniCssExtractPlugin()
+  ],
+  optimization: {
+    minimizer: [
+      '...',
+      new CssMinimizerPlugin()
+    ]
   }
 }
