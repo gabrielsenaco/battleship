0a28602ab0374ec4b623e2a1b5f1f18d18812a13
Gabriel <gabesenacom@gmail.com>
2021-08-11 19:14:36 -0300

    Organize workspace configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,13 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    'jest/globals': true
   },
   extends: ['standard'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module'
   },
-  plugins: ['jest'],
-  env: {
-    'jest/globals': true
-  }
+  plugins: ['jest']
 }
