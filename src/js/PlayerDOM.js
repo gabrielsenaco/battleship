0142b2820a5eed3971c582a8d854c1f81840fbfd
Gabriel <gabesenacom@gmail.com>
2021-08-11 19:25:30 -0300

    Add PlayerDOM module This module contains some functions that helps to group the DOM with the 'game logic'

---
+++ src/js/PlayerDOM.js
@@ -0,0 +1,39 @@
+import PubSub from 'pubsub-js'
+import { TOPIC } from './topics'
+import { buildGameboardDOMByModel } from './GameboardDOM'
+
+export const PlayerGame = (object, bot) => {
+  const enemyGameboardDOM = buildGameboardDOMByModel(
+    false, true,
+    object.getEnemyGameboard()
+  )
+
+  let gameboardDOM
+  if (!bot) {
+    gameboardDOM = buildGameboardDOMByModel(false, false, object.getGameboard())
+    enemyGameboardDOM.grid.addEventListener(
+      'click',
+      listenToGameboardClick.bind(event, object)
+    )
+  }
+
+  return {
+    object,
+    bot,
+    gameboardDOM,
+    enemyGameboardDOM
+  }
+}
+
+function listenToGameboardClick (object, event) {
+  const item = event.target
+  if (item.hasAttribute('data-x') && item.hasAttribute('data-y')) {
+    const x = parseInt(item.getAttribute('data-x'))
+    const y = parseInt(item.getAttribute('data-y'))
+    PubSub.publishSync(TOPIC.SET_PLAYER_OPTION, {
+      x,
+      y,
+      object
+    })
+  }
+}
