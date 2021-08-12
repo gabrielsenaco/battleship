4c0898cc4df0f3b1dd8a7f75f2bcee939660bfb1
Gabriel <gabesenacom@gmail.com>
2021-08-11 22:59:59 -0300

    Remove old code from index.js
    Now, this function will only call 'PING_SETUP_PLAYERS' topic to start game setup

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,9 @@
-import '../css/styles.css'
-import './PassDevice'
-import init from './Game'
-import { Player, ComputerPlayer } from './Player'
-import { getShips } from './Ships'
-import './PlayerSetup'
 import PubSub from 'pubsub-js'
 import { TOPIC } from './topics'
-/*
-  TO-DO
-  [ ] 1. SETUP DOS PLAYERS: NOMES E SE É BOT OU NÃO
-  [ ] 2. SETUP DO ESQUEMA DA GAMEBOARD
-  --APÓS ISSO, CHAMAR A FUNÇÃO #INIT DO MÓDULO INIT COM A LISTA DOS DOIS PLAYERS.
-*/
-PubSub.publish(TOPIC.PING_SETUP_PLAYERS, {})
-const player1 = ComputerPlayer('James')
-const player2 = ComputerPlayer('Pro') // ComputerPlayer
-player1.buildGameboardScheme(getShips())
-player2.buildGameboardScheme(getShips())
-const playerList = [player1, player2]
+import '../css/styles.css'
+import './PassDevice'
+import './Game'
+import './PlayerSetup'
+import './ShipSetup'
 
-init(playerList)
-// let data = buildGameboardDOMByModel(false, player1.getGameboard())
-// markPointInGrid(4,4, data.grid)
+PubSub.publish(TOPIC.PING_SETUP_PLAYERS, {})
