ea89bf35c04982ffc22516eea760c4fb898dc28e
Gabriel <gabesenacom@gmail.com>
2021-08-11 22:58:17 -0300

    Validate if object is null first; Add some useful validator methods.

--- src/js/PlayerValidator.js
+++ src/js/PlayerValidator.js
@@ -5,9 +5,23 @@ const _computerModel = ComputerPlayer()
 const _playerModel = Player()
 
 export function isValidPlayer (object) {
+  if (!object) return false
   return objectsHaveSameKeys(object, _playerModel)
 }
 
 export function isValidComputerPlayer (object) {
+  if (!object) return false
   return objectsHaveSameKeys(object, _computerModel)
+}
+
+export function isAllPlayer (players) {
+  return _isAll(players, isValidPlayer)
+}
+
+export function isAllComputerPlayer (players) {
+  return _isAll(players, isValidComputerPlayer)
+}
+
+function _isAll (players, validator) {
+  return players.every(player => validator(player))
 }
