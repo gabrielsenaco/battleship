7ce6fd121d5cfeffafc0763a6823f2b511176ccb
Gabriel <gabesenacom@gmail.com>
2021-08-14 19:06:22 -0300

    Restructure ShipSetup to look like planned UI

--- src/js/ShipSetup.js
+++ src/js/ShipSetup.js
@@ -4,10 +4,19 @@ import { buildGameboardDOMByModel } from './GameboardDOM'
 import {
   listenSubmitClick,
   listenRotateClick,
-  listenRandomSchemeClick
+  listenRandomSchemeClick,
+  listenResetClick
 } from './ShipSetupListener'
+import {
+  listenDragStart,
+  listenDrop,
+  listenDragOver
+} from './ShipSetupDragDropListener'
+import { setShipComponentImage } from './ShipDOM'
 
 export function buildGameboard (parentNode, player) {
+  const oldGameboard = parentNode.querySelector('.grid-container')
+  if (oldGameboard) oldGameboard.remove()
   const gameboard = buildGameboardDOMByModel(
     true,
     false,
@@ -16,73 +25,99 @@ export function buildGameboard (parentNode, player) {
     false
   )
   parentNode.appendChild(gameboard.container)
+  gameboard.grid.addEventListener('drop', listenDrop.bind(event, player))
+  gameboard.grid.addEventListener(
+    'dragover',
+    listenDragOver.bind(event, player)
+  )
   return gameboard
 }
 
+export function buildShipsContainer (parentNode, orientation) {
+  const oldContainer = document.getElementById('ships-container')
+  if (oldContainer) oldContainer.remove()
+  const container = createElement('div', null, 'ships-container', parentNode)
+  const ships = createElement('div', null, 'ships', container)
+  ships.setAttribute('data-orientation', orientation)
+  getShips().forEach((ship, index) =>
+    _buildShip(ship.name, ship.length, ships, orientation, index)
+  )
+}
+
+export function rotateShipsInContainer (orientation) {
+  const ships = document.getElementById('ships')
+  Array.from(ships.children).forEach(ship => {
+    const length = parseInt(ship.getAttribute('data-length'))
+    Array.from(ship.children).forEach((component, index) =>
+      setShipComponentImage(component, length, index, orientation)
+    )
+  })
+}
+
+function _buildShip (shipName, shipLength, parentNode, orientation, id = 0) {
+  const ship = createElement('div', 'ship', null, parentNode)
+  ship.setAttribute('draggable', 'true')
+  ship.setAttribute('data-length', shipLength)
+  ship.setAttribute('data-id', id)
+  ship.setAttribute('data-name', shipName)
+  ship.addEventListener('dragstart', listenDragStart)
+  for (let i = 0; i < shipLength; i++) {
+    const component = createElement('div', 'ship-component', null, ship)
+    setShipComponentImage(component, shipLength, i, orientation)
+  }
+}
+
+function buildShiSetupTitle (parentNode, player) {
+  const title = createElement('h2', 'title', null, parentNode)
+  title.textContent = `${player.getName()}, setup your ships`
+}
+
+function buildActionButtons (parentNode, player, playerList) {
+  const container = createElement(
+    'div',
+    null,
+    'action-button-container',
+    parentNode
+  )
+  const rotate = createElement('button', 'btn', null, container)
+  const random = createElement('button', 'btn', null, container)
+  const reset = createElement('button', 'btn', null, container)
+  rotate.textContent = 'Rotate'
+  random.textContent = 'Random'
+  reset.textContent = 'Reset'
+  rotate.addEventListener(
+    'click',
+    listenRotateClick.bind(event, player, playerList)
+  )
+  random.addEventListener(
+    'click',
+    listenRandomSchemeClick.bind(event, player, playerList)
+  )
+  reset.addEventListener('click', listenResetClick.bind(event, player))
+}
+
+function buildShipSetupFinishTitle (parentNode, hasNextPlayer) {
+  const title = createElement('h3', 'start-title subtitle', null, parentNode)
+  title.textContent = hasNextPlayer ? 'Start next setup' : 'Start game'
+}
+
+function buildSubmitButton (parentNode, player, playerList, hasNextPlayer) {
+  const button = createElement('button', 'btn start-game-btn confirm-form', null, parentNode)
+  button.textContent = hasNextPlayer ? 'Next setup' : 'Start game'
+  button.addEventListener(
+    'click',
+    listenSubmitClick.bind(event, player, playerList)
+  )
+}
 export const createShipSetup = (player, playerList, hasNextPlayer) => {
   function build () {
     const container = createElement('main', null, 'ships-setup', document.body)
-    buildShiSetupTitle(container)
-    buildShipsContainer(container)
-    buildActionButtons(container)
+    buildShiSetupTitle(container, player)
+    buildShipsContainer(container, 'horizontal')
+    buildActionButtons(container, player, playerList)
     buildGameboard(container, player)
-    buildShiSetupFinishTitle(container)
-    buildSubmitButton(container)
-  }
-
-  function buildShiSetupTitle (parentNode) {
-    const title = createElement('h2', null, null, parentNode)
-    title.textContent = `${player.getName()}, setup your ships`
-  }
-
-  function buildShipsContainer (parentNode) {
-    const container = createElement('div', null, 'ships-container', parentNode)
-    const ships = createElement('div', null, 'ships', container)
-    ships.setAttribute('rotate', 'horizontal')
-    getShips().forEach(ship => buildShip(ship.length, ships))
-  }
-
-  function buildShip (shipLength, parentNode) {
-    const ship = createElement('div', 'ship', null, parentNode)
-    for (let i = 0; i < shipLength; i++) {
-      createElement('div', 'ship-component', null, ship)
-    }
-  }
-
-  function buildActionButtons (parentNode) {
-    const container = createElement(
-      'div',
-      null,
-      'action-button-container',
-      parentNode
-    )
-    const rotate = createElement('button', 'btn', null, container)
-    // TODO ADICIONAR ICONES
-    rotate.textContent = 'Rotate'
-    const random = createElement('button', 'btn', null, container)
-    random.textContent = 'Random'
-    rotate.addEventListener(
-      'click',
-      listenRotateClick.bind(event, player, playerList)
-    )
-    random.addEventListener(
-      'click',
-      listenRandomSchemeClick.bind(event, player, playerList)
-    )
-  }
-
-  function buildShiSetupFinishTitle (parentNode) {
-    const title = createElement('h3', null, null, parentNode)
-    title.textContent = hasNextPlayer ? 'Start next setup' : 'Start game'
-  }
-
-  function buildSubmitButton (parentNode) {
-    const button = createElement('button', 'btn', null, parentNode)
-    button.textContent = hasNextPlayer ? 'Next setup' : 'Start game'
-    button.addEventListener(
-      'click',
-      listenSubmitClick.bind(event, player, playerList)
-    )
+    buildShipSetupFinishTitle(container, hasNextPlayer)
+    buildSubmitButton(container, player, playerList, hasNextPlayer)
   }
 
   build()
