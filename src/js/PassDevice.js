8928c56e8739ffe2bd6eed59c8119b03b8e4df65
Gabriel <gabesenacom@gmail.com>
2021-08-14 21:35:54 -0300

    Clear PassDevice code

--- src/js/PassDevice.js
+++ src/js/PassDevice.js
@@ -4,15 +4,21 @@ import { createElement } from './utils'
 
 const createPassDevicePage = player => {
   const name = player.object.getName()
-  const container = createElement('section', null, 'pass-device', document.body)
-  const text = createElement('p', 'pass-device-text', null, container)
-  text.textContent = `Now it's ${name}'s turn. Pass the device to him/her. It is necessary for the player to confirm by clicking the button below.`
-  const passButton = createElement(
-    'button',
-    'btn',
-    'pass-device-btn',
-    container
+  const container = createElement(
+    'section',
+    'page',
+    'pass-device',
+    document.body
   )
+  createElement(
+    'p',
+    'page-text',
+    null,
+    container
+  ).textContent = `Now it's ${name}'s turn. Pass the device to him/her.`
+  createElement('p', 'page-text', null, container).textContent =
+    'It is necessary for the player to confirm by clicking the button below.'
+  const passButton = createElement('button', 'btn page-btn', null, container)
   passButton.textContent = 'Device passed to me'
   passButton.addEventListener('click', () => {
     container.remove()
